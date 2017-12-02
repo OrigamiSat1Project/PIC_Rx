38d80146771256e1337f8ef954fb7be3e913ebe1
KosukeIkeya <kosukeikeya50@gmail.com>
2017-12-03 04:07:37 +0900

    change BOREN & BOR4V

--- PIC_Rx/main.c
+++ PIC_Rx/main.c
@@ -28,11 +28,11 @@
 #pragma config MCLRE    = ON            // RE3/MCLR pin function select bit (RE3/MCLR pin function is MCLR)
 #pragma config CP       = OFF           // Code Protection bit (Program memory code protection is disabled)
 #pragma config CPD      = OFF           // Data Code Protection bit (Data memory code protection is disabled)
-#pragma config BOREN    = OFF            // Brown Out Reset Selection bits (BOR enabled)
+#pragma config BOREN    = ON            // Brown Out Reset Selection bits (BOR enabled)
 #pragma config IESO     = OFF            // Internal External Switchover bit (Internal/External Switchover mode is enabled)
 #pragma config FCMEN    = OFF            // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is enabled)
 #pragma config LVP      = OFF           // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
-#pragma config BOR4V    = BOR40V        // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config BOR4V    = BOR21V        // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
 #pragma config WRT      = OFF           // Flash Program Memory Self Write Enable bits (Write protection off)
 
 void main(void) {
