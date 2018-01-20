73abce1fd2c7d37665e5a3d9e0bacf68dd065d19
KosukeIkeya <kosukeikeya50@gmail.com>
2018-01-20 21:26:35 +0900

    add WDT.h & WDT.c

---
+++ PIC_Tx/WDT.c
@@ -0,0 +1,42 @@
+#include <xc.h>
+#include "WDT.h"
+#include "Type_define.h"
+
+void Init_WDT(void){
+    INTCONbits.GIE  = 1;    // Grobal interrupt??????
+    INTCONbits.PEIE = 1;    // ???????????
+ 
+    PIR1bits.TMR1IF = 0;    // Timer1??????????????
+    PIE1bits.TMR1IE = 1;    // Timer1???????
+ 
+ 
+    T1CONbits.T1CKPS = 2;   // ???????1/1???
+    TMR1 = TIMER_INTERVAL;  // ????????????
+ 
+    T1CONbits.TMR1ON = 1;   // Timer1??????
+}
+
+
+
+//void interrupt intr(void){
+//    volatile static int intr_counter;
+//    if ( PIR1bits.TMR1IF == 1 ) {
+//        TMR1 = TIMER_INTERVAL;  // ?????????
+// 
+//        intr_counter++;
+//        if ( intr_counter >= 100 ) {
+//            intr_counter = 0;
+//        }
+// 
+//        // 0.5sec???RB0???????    
+//        if ( intr_counter <= 50 || intr_counter > 51) {
+//            PORTAbits.RA0 = 1;
+//        } else {
+//            PORTAbits.RA0 = 0;
+//        }
+// 
+//        PIR1bits.TMR1IF = 0;    // ???????????
+//    }   
+// 
+//    return;
+//}
