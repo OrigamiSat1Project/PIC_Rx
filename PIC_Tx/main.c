47194f5f1b0cc57b5d2ed6729dc226fe61448e18
MadokaKurosaki <kuroyagimaa@gmail.com>
2018-09-08 12:57:37 +0900

    eliminate errors

--- PIC_Tx/main.c
+++ PIC_Tx/main.c
@@ -172,24 +172,24 @@ void interrupt InterReceiver(void){
         }
         RCIF = 0;
     }
-    if ( PIR1bits.TMR1IF == 1 ) {
-        TMR1 = TIMER_INTERVAL;  // ?????????
- 
-        intr_counter++;
-        if ( intr_counter >= 100 ) {
-            intr_counter = 0;
-        }
-         //0.5sec???RB0???????    
-        if ( intr_counter <= 50 || intr_counter > 51) {
-//            PORTAbits.RA0 = 1;
-        } else {
-//            PORTAbits.RA0 = 0;
-        }
- 
-        PIR1bits.TMR1IF = 0;    // ???????????
-    }   
- 
-    return;
+//    if ( PIR1bits.TMR1IF == 1 ) {
+//        TMR1 = TIMER_INTERVAL;  // ?????????
+// 
+//        intr_counter++;
+//        if ( intr_counter >= 100 ) {
+//            intr_counter = 0;
+//        }
+//         //0.5sec???RB0???????    
+//        if ( intr_counter <= 50 || intr_counter > 51) {
+////            PORTAbits.RA0 = 1;
+//        } else {
+////            PORTAbits.RA0 = 0;
+//        }
+// 
+//        PIR1bits.TMR1IF = 0;    // ???????????
+//    }   
+// 
+//    return;
 }
 
 
